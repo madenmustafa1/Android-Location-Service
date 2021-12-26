33c9fb832303e4ccfeddf87403595d29b9439e87
Mustafa Maden <67011777+madenmustafa1@users.noreply.github.com>
2021-12-26 12:21:44 +0300

    Location Service

---
+++ LocationServiceK.kt
@@ -0,0 +1,129 @@
+package your package
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.lang.Exception
+import java.lang.UnsupportedOperationException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class LocationServiceK : Service() {
+    private val locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+
+            try {
+                if (locationResult != null && locationResult.lastLocation != null) {
+                    val latitude = locationResult.lastLocation.latitude
+                    val longitude = locationResult.lastLocation.longitude
+                    //println("Location KService: $latitude $longitude")
+
+
+
+                }
+            } catch (e: Exception) {
+                println("Service Error 81: " + e.message)
+
+                //Firebase.crashlytics.recordException(e)
+            }
+
+        }
+    }
+
+
+    override fun onBind(intent: Intent): IBinder? {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationService() {
+        val channelId = "location_notification_channel"
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val resultIntent = Intent()
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            channelId
+        )
+        builder.setSmallIcon("your app icon")
+        builder.setContentTitle("Location service")
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL)
+        builder.setContentText("Running")
+        builder.setContentIntent(pendingIntent)
+        builder.setAutoCancel(false)
+        builder.priority = NotificationCompat.PRIORITY_MAX
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
+                val notificationChannel = NotificationChannel(
+                    channelId,
+                    "Location Service",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                notificationChannel.description = "This channel is used by service"
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
+        }
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 60000
+        locationRequest.fastestInterval = 48000
+        //locationRequest.interval = 4000 -- 60000
+        //locationRequest.fastestInterval = 2000 -- 48000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        //PRIORITY_HIGH_ACCURACY
+
+
+        /*
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+         */LocationServices.getFusedLocationProviderClient(this)
+            .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        startForeground(LocationServiceConstants.LOCATION_SERVICE_ID, builder.build())
+    }
+
+    private fun stopLocationService() {
+        LocationServices.getFusedLocationProviderClient(this)
+            .removeLocationUpdates(locationCallback)
+        stopForeground(true)
+        stopSelf()
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            val action = intent.action
+            if (action != null) {
+                if (action == LocationServiceConstants.ACTION_START_LOCATION_SERVICE) {
+                    startLocationService()
+                } else if (action == LocationServiceConstants.ACTION_STOP_LOCATION_SERVICE) {
+                    stopLocationService()
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
---
+++ MainActivity.kt
@@ -0,0 +1,56 @@
+package "your package"
+
+class MainActivity : AppCompatActivity(){
+ 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      
+
+        stopLocationService()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        startLocationService()
+    }
+
+    private fun isLocationServiceRunning(): Boolean {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        if (activityManager != null) {
+            for (service in activityManager.getRunningServices(Int.MAX_VALUE)) {
+                if (LocationServiceK::class.java.name == service.service.className) {
+                    if (service.foreground) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+        return false
+    }
+
+    private fun startLocationService() {
+        if (!isLocationServiceRunning()) {
+            val intent = Intent(
+                applicationContext,
+                LocationServiceK::class.java
+            )
+            intent.action = "startLocationService"
+            startService(intent)
+            Toast.makeText(applicationContext, "LocationService Start", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun stopLocationService() {
+        if (isLocationServiceRunning()) {
+            val intent = Intent(
+                applicationContext,
+                LocationServiceK::class.java
+            )
+            intent.action = "stopLocationService";
+            startService(intent)
+            Toast.makeText(applicationContext, "Location service stop", Toast.LENGTH_LONG).show()
+        }
+    }
+}
